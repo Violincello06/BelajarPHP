8faf9d3efcdc586f3ab1c44b29e745e79a865a14
Ralip Pranaja Violincello <pranaja0852@gmail.com>
2026-04-23 14:13:18 +0700

    penambahan tugas 2

---
+++ tugas2.php
@@ -0,0 +1,11 @@
+<?php
+$pi = 3.14;
+$r = 7;
+$s = 25;
+
+$luasAlas = $pi * $r * $r;
+$luasPermukaan = ($pi * $r * $r) + ($pi * $r * $s);
+
+echo "Luas Alas        : " . $luasAlas . " cm2 <br>";
+echo "Luas Permukaan   : " . $luasPermukaan . " cm2 <br>";
+?>
