d473cdbad7f105468b48337969b42ef89b522192
Ralip Pranaja Violincello <pranaja0852@gmail.com>
2026-04-23 14:00:49 +0700

    Penambahan file tugas1

---
+++ tugas1.php
@@ -0,0 +1,11 @@
+<?php
+$saldoAwal = 2000000;
+$bunga = 0.03;
+$bulan = 11;
+$saldoAkhir = $saldoAwal + ($saldoAwal * $bunga * $bulan);
+echo "Saldo Awal        : Rp. " . $saldoAwal . ",-<br>";
+echo "Bunga per bulan   : " . ($bunga * 100) . "%<br>";
+echo "Lama menabung     : " . $bulan . " bulan<br>";
+echo "Total bunga       : Rp. " . ($saldoAwal * $bunga * $bulan) . ",-<br>";
+echo "Saldo akhir setelah " . $bulan . " bulan adalah : Rp. " . $saldoAkhir . ",-";
+?>
