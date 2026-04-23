1e8dec2d97558d0b2bf158c67f93c140751f8e98
Ralip Pranaja Violincello <pranaja0852@gmail.com>
2026-04-23 13:35:41 +0700

    Penambahan  file biodata

---
+++ biodata.php
@@ -0,0 +1,10 @@
+<?php
+    $nim = '1202407013';
+    $nama = 'Ralip Pranaja Violincello';
+    $alamat = 'jl. Suka Maju No. 123';
+    $nilai = 90;
+    echo "NIM : ", $nim, "<br>";
+    echo "Nama : ", $nama, "<br>";
+    echo "Alamat : ", $alamat, "<br>";
+    echo "Nilai : $nilai";
+?>
