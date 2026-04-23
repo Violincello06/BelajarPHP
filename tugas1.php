90e5df53f80c504019fe8a6ce0aaa91bf4c7d8fd
Ralip Pranaja Violincello <pranaja0852@gmail.com>
2026-04-23 15:05:14 +0700

    edit dikit

--- tugas1.php
+++ tugas1.php
@@ -3,6 +3,7 @@ $saldoAwal = 2000000;
 $bunga = 0.03;
 $bulan = 11;
 $saldoAkhir = $saldoAwal + ($saldoAwal * $bunga * $bulan);
+
 echo "Saldo Awal        : Rp. " . $saldoAwal . ",-<br>";
 echo "Bunga per bulan   : " . ($bunga * 100) . "%<br>";
 echo "Lama menabung     : " . $bulan . " bulan<br>";
