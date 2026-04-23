65d0bcc7c06f6a7b4627df0420f3d678ebb6931a
Ralip Pranaja Violincello <pranaja0852@gmail.com>
2026-04-23 13:46:05 +0700

    penambahan file operasi

---
+++ operasi.php
@@ -0,0 +1,23 @@
+<?php
+$a=5;
+$b=2.5;
+$komentar="Selamat Datang...Kicau Kicau Kicau Mania";
+echo("Nilai Variabel a : $a <br>");
+echo("Nilai Variabel b : $b <br>");
+echo("Nilai Variabel komentar : $komentar <br>");
+$tambah = $a + $b;
+$kurang = $a - $b;
+$pembagian = $a / $b;
+$perkalian = $a * $b;
+echo("Hasil Penjumlahan a + b : $tambah <br>");
+echo("Hasil Pengurangan a - b : $kurang <br>");
+echo("Hasil Pembagian a / b : $pembagian <br>");
+echo("Hasil Perkalian a * b : $perkalian <br>");
+$nama="UMPKU";
+$garis = "==============================";
+echo "<p>";
+echo $garis."<br>";
+echo $komentar. " Di Lab ". $nama. "<br>Selamat Belajar Pemrograman
+Web <br>";
+echo $garis."<br>"; 
+?>
